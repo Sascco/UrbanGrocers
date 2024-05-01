5fa0d69ae930b9e7a83c52d3e004649f9a2fe133
Sascco <92239099+Sascco@users.noreply.github.com>
2024-05-01 09:10:59 -0500

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = 'https://cnt-100e0eb6-1aff-4379-af74-86d1a2a33942.containerhub.tripleten-services.com'
+URL_SERVICE = 'https://cnt-c67524be-3356-4ba7-baa4-9dbbbd2bfe54.containerhub.tripleten-services.com'
 CREATE_USER_PATH = "/api/v1/users/"# Almacena la ruta para crear un usuario o usuaria en esta variable
 CREATE_KITS_PATH = "/api/v1/kits" # Almacena la ruta para crear un kit en esta variable
